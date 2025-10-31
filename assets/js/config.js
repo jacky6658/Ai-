28621a0eb72bcb333330922fb4fab333735ae83a
jack <aiagentg888@gmail.com>
2025-10-31 12:19:07 +0800

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+// 全域設定（保持與現有專案一致）
+window.APP_CONFIG = {
+  API_BASE: 'https://aivideobackend.zeabur.app',
+  ACCESS_TOKEN_KEY: 'ipPlanningToken',
+  ACCESS_TOKEN_UPDATED_AT: 'ipPlanningTokenUpdated',
+  REFRESH_TOKEN_KEY: 'ipPlanningRefreshToken'
+};
+
+
